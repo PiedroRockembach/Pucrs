1810af29233de8eed2c8211d41556faad8b3f349
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2024-03-15 19:46:32 -0300

    realização do exercício 5 da lista 2

---
+++ 15-03/lista_2_ex5.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class sale_price{
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("\fInforme o preço de custo: ");
+        double price = teclado.nextDouble
