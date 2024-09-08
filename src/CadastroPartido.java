fb9c9d6626228fee40b3d72e623060446b9adbfd
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2024-09-08 19:09:30 -0300

    push inital files

---
+++ src/CadastroPartido.java
@@ -0,0 +1,19 @@
+import java.util.Collection;
+
+public class CadastroPartido {
+
+	private Collection<Partido> partido;
+
+	public boolean cadastraPartido(Partido p) {
+		return false;
+	}
+
+	public Partido consultaPartido(String nome) {
+		return null;
+	}
+
+	public Partido consultaPartido(int numero) {
+		return null;
+	}
+
+}
---
+++ src/Candidatura.java
@@ -0,0 +1,11 @@
+import java.util.Collection;
+
+public class Candidatura {
+
+	private Collection<Candidato> candidato;
+
+	public boolean cadastraCandidato(Candidato c) {
+		return false;
+	}
+
+}
