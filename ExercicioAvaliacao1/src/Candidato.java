e44ef764a841060a72296383e5a310721dd46bb9
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2024-09-09 14:59:39 -0300

    add initial

---
+++ ExercicioAvaliacao1/src/Candidato.java
@@ -0,0 +1,18 @@
+public class Candidato {
+
+	private int numero;
+
+	private String nome;
+
+	private String municipio;
+
+	private int votos;
+	
+	public Candidato(int numero, String nome, String municipio) {
+		this.numero = numero;
+		this.nome = nome;
+		this.municipio = municipio;
+		this.votos = 0;
+	}
+
+}
