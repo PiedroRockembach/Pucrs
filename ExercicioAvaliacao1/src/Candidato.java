5aa7c5323d2b92a218bedffb66f7dc9ebe70f9db
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2024-09-09 15:45:25 -0300

    candidatura

--- ExercicioAvaliacao1/src/Candidato.java
+++ ExercicioAvaliacao1/src/Candidato.java
@@ -7,6 +7,18 @@ public class Candidato {
 	private String municipio;
 
 	private int votos;
+	public int getNumero() {
+		return numero;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public int getVotos() {
+		return votos;
+	}
 	
 	public Candidato(int numero, String nome, String municipio) {
 		this.numero = numero;
--- ExercicioAvaliacao1/src/Candidatura.java
+++ ExercicioAvaliacao1/src/Candidatura.java
@@ -5,6 +5,11 @@ public class Candidatura {
 	private Collection<Candidato> candidato;
 
 	public boolean cadastraCandidato(Candidato c) {
+		for (Candidato candidato : candidato) {
+			if (candidato.getNumero() == c.getNumero()  && candidato.getMunicipio().equals(c.getMunicipio())) {
+				return false;
+			}
+		}
 		return false;
 	}
 
