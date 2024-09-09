5e8e28c1ea301889dd8f51602a24c919b6492f89
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2024-09-09 15:40:48 -0300

    cadastro partido

--- ExercicioAvaliacao1/src/CadastroPartido.java
+++ ExercicioAvaliacao1/src/CadastroPartido.java
@@ -1,18 +1,34 @@
-import java.util.Collection;
+import java.util.ArrayList;
 
 public class CadastroPartido {
 
-	private Collection<Partido> partido;
+	private ArrayList<Partido> partido;
 
 	public boolean cadastraPartido(Partido p) {
-		return false;
+		for (Partido partido : partido) {
+			if (partido.getNumero() == p.getNumero()) {
+				return false;
+			}
+		}
+		partido.add(p);
+		return true;
 	}
 
 	public Partido consultaPartido(String nome) {
+		for (Partido partido : partido) {
+			if (partido.getNome().equals(nome)) {
+				return partido;
+			}
+		}
 		return null;
 	}
 
 	public Partido consultaPartido(int numero) {
+		for (Partido partido : partido) {
+			if (partido.getNumero() == numero) {
+				return partido;
+			}
+		}
 		return null;
 	}
 
