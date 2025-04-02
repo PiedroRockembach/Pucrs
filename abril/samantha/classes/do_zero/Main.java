d60ad37e0485e9f843312ec9efea2340dca21257
Piedro Rockembach <79763019+PiedroRockembach@users.noreply.github.com>
2025-04-02 17:43:32 -0300

    create the first samantha's april exercise

---
+++ abril/samantha/classes/do_zero/Main.java
@@ -0,0 +1,27 @@
+package abril.samantha.classes.do_zero;
+
+/**
+
+ Uma petshop precisa de um sistema para saber o dono de um pet através do número da coleira
+ O sistema irá armazenar o nome e o cpf de cada cliente, 
+ para o pet será necessário armazenar seu nome e seu respectivo dono
+ e é preciso que seja possível:
+
+ - consultar o dono de um determinado pet
+ - listar todos os pets de um determinado dono
+ 
+ Não é necessário a construção de um menu, apenas:
+ - crie ao menos 3 clientes e 5 pets.
+ - execute ao menos 2 exemplos de cada uma das operações descritas acima.
+
+- Você pode desenvolver a solução da forma que quiser.
+- Você pode criar quantas classes e métodos desejar.
+- Execute o exercício na classe abaixo.
+ */
+
+public class Main{
+    public static void main(String[] args) {
+        // Desenvolva a sua solução aqui.
+
+    }
+}
